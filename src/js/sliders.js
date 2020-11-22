47dd1942d3a92e6bf28573a52aca355d04b5f8ff
testtimshaq <58642417+testtimshaq@users.noreply.github.com>
2020-11-22 19:38:22 +0500

    prod slider

--- src/js/sliders.js
+++ src/js/sliders.js
@@ -6,6 +6,7 @@ export default function() {
 
 
 const previewSlider = new Swiper('.preview.swiper-container', {
+  width: 1170,
   slidesPerView: 1,
   loopedSlides: 1,
   spaceBetween: 50,
@@ -18,8 +19,22 @@ const previewSlider = new Swiper('.preview.swiper-container', {
 
 })
 
+const productLineSliders = new Swiper('.product-line .product-line__slider .swiper-container', {
+	width: 1170,
+	slidesPerView: 4,
+	slidesPerColumn: 1,
+	spaceBetween: 30,
+	allowTouchMove: false,
+	navigation: {
+		nextEl: '.product-line .slider-controls .slider-controls__btn.next',
+		prevEl: '.product-line .slider-controls .slider-controls__btn.prev',
+		hideOnClick: true,
+	},
+})
+
 const productLineInsideSliders = new Swiper('.product-line__phcont-slider .swiper-container', {
   slidesPerView: 1,
+  slideClass: 'swiper-slide-inside',
   pagination: {
     el: '.swiper-pagination',
     type: 'bullets',
@@ -28,24 +43,24 @@ const productLineInsideSliders = new Swiper('.product-line__phcont-slider .swipe
 })
 
 const brandsSlider = new Swiper('.brands__slider', {
-  slidesPerView: 6,
-  slidesPerColumn: 1,
-  navigation: {
-    nextEl: '.brands .slider-controls .slider-controls__btn.next',
-    prevEl: '.brands .slider-controls .slider-controls__btn.prev',
-    hideOnClick: true,
-  },
+	slidesPerView: 6,
+	slidesPerColumn: 1,
+	navigation: {
+		nextEl: '.brands .slider-controls .slider-controls__btn.next',
+		prevEl: '.brands .slider-controls .slider-controls__btn.prev',
+		hideOnClick: true,
+	},
 
 })
 
 const salesSlider = new Swiper('.sales .sales__col.slider .swiper-container', {
-  slidesPerView: 2,
-  spaceBetween: 30,
-  navigation: {
-    nextEl: '.sales .sales__col.slider .sales__slider-arrow.next',
-    prevEl: '.sales .sales__col.slider .sales__slider-arrow.prev',
-    hideOnClick: true,
-  },
+	slidesPerView: 2,
+	spaceBetween: 30,
+	navigation: {
+		nextEl: '.sales .sales__col.slider .sales__slider-arrow.next',
+		prevEl: '.sales .sales__col.slider .sales__slider-arrow.prev',
+		hideOnClick: true,
+	},
 
 })
 
